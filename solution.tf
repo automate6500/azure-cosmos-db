2932ea193963b3e6742636d0f2c0f74d0b24eb16
Michael Jenkins <managedkaos@users.noreply.github.com>
2021-03-22 19:45:49 -0700

    Add solution

---
+++ solution.tf
@@ -0,0 +1,95 @@
+resource "azurerm_cosmosdb_account" "lab" {
+  name                = azurerm_resource_group.lab.name
+  resource_group_name = azurerm_resource_group.lab.name
+  location            = azurerm_resource_group.lab.location
+  offer_type          = "Standard"
+  enable_free_tier    = true
+
+  capabilities {
+    name = "EnableCassandra"
+  }
+
+  consistency_policy {
+    consistency_level       = "Session"
+    max_interval_in_seconds = 10
+    max_staleness_prefix    = 200
+  }
+
+  geo_location {
+    location          = azurerm_resource_group.lab.location
+    failover_priority = 0
+  }
+}
+
+resource "azurerm_cosmosdb_cassandra_keyspace" "lab" {
+  name                = "customers"
+  resource_group_name = azurerm_resource_group.lab.name
+  account_name        = azurerm_cosmosdb_account.lab.name
+  throughput          = 400
+}
+
+resource "azurerm_cosmosdb_cassandra_table" "lab" {
+  name                  = "records"
+  cassandra_keyspace_id = azurerm_cosmosdb_cassandra_keyspace.lab.id
+
+  schema {
+    partition_key {
+      name = "id"
+    }
+
+    column {
+      name = "id"
+      type = "int"
+    }
+
+    column {
+      name = "address"
+      type = "ascii"
+    }
+
+    column {
+      name = "blood_group"
+      type = "ascii"
+    }
+
+    column {
+      name = "company"
+      type = "ascii"
+    }
+
+    column {
+      name = "job"
+      type = "ascii"
+    }
+
+    column {
+      name = "mail"
+      type = "ascii"
+    }
+
+    column {
+      name = "name"
+      type = "ascii"
+    }
+
+    column {
+      name = "residence"
+      type = "ascii"
+    }
+
+    column {
+      name = "sex"
+      type = "ascii"
+    }
+
+    column {
+      name = "ssn"
+      type = "ascii"
+    }
+
+    column {
+      name = "username"
+      type = "ascii"
+    }
+  }
+}
